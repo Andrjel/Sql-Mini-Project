ad9ac24a054c380663d07e298c215b0bd652a4be
Marcin Kazmierczak <km48913@zut.edu.pl>
2024-03-25 22:05:56 +0100

    Sync Polygon.io wrapper works

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 from dbConnection import DbConnection
-from client import Client
+from client import ClientSync
 
 
 def main():
-    c = Client()
-    result = c.get_all_tickers()
+    c = ClientSync()
+    # result = c.get_all_tickers()
+    # result = c.get_all_ticker_types()
+    # result = c.get_grouped_daily("2024-03-22")
+    # result = c.get_stock_dividend("NVDA")
+    # result = c.get_aggregate_bars("MSFT", 1, "day", "2023-01-09", "2024-03-22")
     print(result)
+    # c.get_all_ticker_types()
 
 
 if __name__ == "__main__":
